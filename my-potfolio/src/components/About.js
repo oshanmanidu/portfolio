91fdf9f0fdbbdbb03494abd2c44836025b2e26bc
oshanmanidu <oshanmanidu12@gmail.com>
2025-08-20 18:15:22 +0530

    3rd

---
+++ my-potfolio/src/components/About.js
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>I’m a passionate developer skilled in React and web technologies.</p>
+    </section>
+  );
+}
+
+export default About;
