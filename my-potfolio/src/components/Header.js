3dfeda7b2874f9bc39cb47731a94d2dfe318078c
oshanmanidu <oshanmanidu12@gmail.com>
2025-08-20 18:24:50 +0530

    tail2

---
+++ my-potfolio/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="text-center p-8 bg-gray-100 rounded-2xl shadow">
+      <h1 className="text-3xl font-bold">Oshan Manidu</h1>
+      <p className="text-gray-600 mt-2">Full Stack Developer</p>
+    </header>
+  );
+}
+
+export default Header;
